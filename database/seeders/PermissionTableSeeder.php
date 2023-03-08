71a99c3da32f2e09bdc31b0e739e22cd0e886f5c
Pobitrodeb <pobitrodn@gmail.com>
2023-03-08 21:29:25 +0600

    Admin User Seeder added '

--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -20,6 +20,7 @@ class PermissionTableSeeder extends Seeder
            'role-create',
            'role-edit',
            'role-delete',
+           
            'product-list',
            'product-create',
            'product-edit',
